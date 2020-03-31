29cf635d6ce27312b5f01182acd3733ff8a062f3
dcharles <pdanielcharles@gmail.com>
2020-03-30 21:58:32 -0700

    Add ability to check word list for spelling.

--- game-board/src/word_list.py
+++ game-board/src/word_list.py
@@ -1,5 +1,6 @@
 import csv
 import random
+from autocorrect import Speller
 
 words = []
 with open('game-board/resources/wordlist.csv') as wordlist_csv:
@@ -15,3 +16,9 @@ def get_word_set(game_number):
   start = game_number * cards_per_game
   end = start + cards_per_game
   return words[start:end]
+
+if __name__== "__main__":
+  spell = Speller(lang='en')
+  for idx, word in enumerate(words):
+    if word != spell(word):
+      input(f'[{idx} of {len(words)}] Perhaps {word} should actually be {spell(word)}?')
