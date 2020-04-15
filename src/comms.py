c6bcdd606bbc87d90edfa11c01c48f9990d4d6da
dcharles <pdanielcharles@gmail.com>
2020-04-14 22:28:10 -0700

    Read "from" number from environment.

--- src/comms.py
+++ src/comms.py
@@ -4,7 +4,7 @@ import os
 account_sid = os.environ['TWILIO_SID']
 auth_token = os.environ['TWILIO_TOKEN']
 client = Client(account_sid, auth_token)
-from_num = '+12058435812'
+from_num = os.environ['TWILIO_NUMBER']
 
 def send_mms(to_num, msg, url):
   client.messages \
